13d216834205e33089b30f58b02cb580884b89f5
Raina Lopes <lopesraina1@gmail.com>
2017-08-10 00:01:59 -0700

    Add files via upload

---
+++ f3.R
@@ -0,0 +1,7 @@
+v <- 20   # is valid
+v <- p + q  # is v alid
+x = 10   # is valid
+10 = t1  # is invalid
+100 -> z  # is also valid
+
+x =y = 300 # is also valid
---
+++ f4.R
@@ -0,0 +1,17 @@
+v <- TRUE 
+print(class(v))
+#Numeric  12.3, 5, 999  
+v <- 23.5
+print(class(v))
+#Integer  2L, 34L, 0L  
+v <- 2L
+print(class(v))
+#Complex  3 + 2i  
+v <- 2+5i
+print(class(v))
+#Character  'a' , '"good", "TRUE", '23.4'  
+v <- "TRUE"
+print(class(v))
+#Raw  "Hello" is stored as 48 65 6c 6c 6f  
+v <- charToRaw("Hello")
+print(class(v))
